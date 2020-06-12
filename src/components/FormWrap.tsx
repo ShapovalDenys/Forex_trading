bcb201a5e0147b32418cb92b71ddf3292b5fdbd6
ShapovalDenys <denys.igorevich@gmail.com>
2020-06-12 10:46:00 +0300

    fix form

--- src/components/FormWrap.tsx
+++ src/components/FormWrap.tsx
@@ -14,12 +14,12 @@ const FormWrap = () => (
         <h3>PLEASE FILL OUT THE FORM BELOW</h3>
         <form method="POST" action="#">
           <div className="form-input__initials">
-            <input className="form-input__name" type="name" name="name" placeholder="First name" maxLength={20} />
-            <input className="form-input__lastName" type="lastName" name="lastName" placeholder="Last name" maxLength={20} />
+            <input className="form-input__name" type="name" name="name" placeholder="First name" maxLength={20} required/>
+            <input className="form-input__lastName" type="lastName" name="lastName" placeholder="Last name" maxLength={20} required/>
           </div>
-          <input type="email" name="email" placeholder="Email" className="form-input__email" maxLength={20} />
-          <input type="phone" name="phone" placeholder="Cell phone number" className="form-input__phone" maxLength={20} />
-          <textarea name="message" placeholder="WHAT`S YOUR QUESTION, COMMENT OR ISUUE?" className="form-input__textarea" maxLength={60} />
+          <input type="email" name="email" placeholder="Email" className="form-input__email" maxLength={20} required/>
+          <input type="phone" name="phone" placeholder="Cell phone number" className="form-input__phone" maxLength={20} required/>
+          <textarea name="message" placeholder="WHAT`S YOUR QUESTION, COMMENT OR ISUUE?" className="form-input__textarea" maxLength={80} />
           <button type="submit" className="form-input__button">SEND THIS MESSAGE</button>
         </form>
       </div>
